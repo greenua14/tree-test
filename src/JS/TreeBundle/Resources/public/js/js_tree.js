0608d7b9483d10eef8b034ddb82465156683f9ce
green <greenua14@gmail.com>
2015-03-29 22:56:46 +0300

    fix js tree

--- src/JS/TreeBundle/Resources/public/js/js_tree.js
+++ src/JS/TreeBundle/Resources/public/js/js_tree.js
@@ -1,20 +1,51 @@
+var data = [];
+
+//var data = [
+//    {
+//        label: 'node1', id: 1,
+//        children: [
+//            { label: 'child1', id: 2 },
+//            { label: 'child2', id: 3 }
+//        ]
+//    },
+//    {
+//        label: 'node2', id: 4,
+//        children: [
+//            { label: 'child3', id: 5 }
+//        ]
+//    }
+//];
+
 var builddata = function (n) {
     var source = [];
-    var item = [];
-    for (var i = 1, j = 1; i <= n, j <= 5; i++, j++)
+
+//    var labels_id = [];
+//    var nodes_labels = [];
+    var child_labels = [];
+    for(var i = 0; i < n * 3; i++)
     {
-        var item = { label: 'node' + i.toString(), id: i,
-            children: [
-                { label: 'child' + j.toString(), id: ++i }
-            ]
+//        labels_id = i;
+
+        for(var j = 0; j < n; j++)
+        {
+//            nodes_labels = 'node' + j.toString();
+
+            for(var k = 0; k < 3; k++)
+            {
+//                child_labels = 'child' + k.toString();
+                source[j] = { label: 'node' + j.toString(), id: i,
+                    children: {
+                        label: 'child' + k.toString(), id: i+1
+                    }
+                }
+            }
         }
-        console.log(item);
-        source[i] = item[i];
     }
+
     return source;
 }
 
-var data = builddata(15);
+data = builddata(10);
 
 $('#tree1').tree({
     data: data,
